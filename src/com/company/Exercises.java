64f94a629f67b72ffe8f1162eaf8baa6a34f5f5a
fmf06 <fmf06pta@gmail.com>
2022-05-07 02:31:42 +0300

    Решение задач для списка из чисел и списка из строк.

--- src/com/company/Exercises.java
+++ src/com/company/Exercises.java
@@ -71,6 +71,158 @@ public class Exercises {
             list.getHead().setNextNode(null);
             list.setHead(lastNode);
         }
+    }
 
+// Метод определения количества слов, которые начинаются и заканчиваются на одну букву
+    public static void task05(ListString list) {
+        int count = 0;
+        NodeString tmpNode = list.getHead();
+        while (tmpNode != null) {
+            if (isMatch(tmpNode.getValue())) {
+                count++;
+            }
+            tmpNode = tmpNode.getNextNode();
+        }
+        System.out.println("Количество слов, которые начинаются и заканчиваются на одну букву, равно " + count);
+    }
+
+    private static boolean isMatch(String str) {
+        return str.toLowerCase().charAt(0) == str.toLowerCase().charAt(str.length() - 1);
+    }
+
+// Метод проверки, что каждое следующее слово в списке начинается с последней буквы предыдущего
+    public static void tack06(ListString list) {
+        NodeString tmpNode = list.getHead();
+        boolean isCheck = false;
+        while (tmpNode.getNextNode()!= null) {
+            if (isNext(tmpNode.getValue(), tmpNode.getNextNode().getValue())) {
+                isCheck = true;
+            } else {
+                isCheck = false;
+                break;
+            }
+            tmpNode = tmpNode.getNextNode();
+        }
+
+        if (isCheck) {
+            System.out.println("Каждое следующее слово в списке начинается с последней буквы предыдущего");
+        } else {
+            System.out.println("В списке есть слово " + tmpNode.getValue() +", которое не оканчивается на первую букву следующего слова");
+        }
+    }
+
+    static boolean isNext(String previous, String next) {
+        return previous.toLowerCase().charAt(previous.length() - 1) == next.toLowerCase().charAt(0);
+    }
+
+// Метод определения количества слов в списке, которые совпадают с первым (последним) словом списка
+    public static void task07(ListString list) {
+        int count = 0;
+        NodeString tmpNode = list.getHead().getNextNode();
+        while (tmpNode != null) {
+            if (list.getHead().getValue().equalsIgnoreCase(tmpNode.getValue())) {
+                count++;
+            }
+            tmpNode = tmpNode.getNextNode();
+        }
+        System.out.println("Количество слов в списке, которые совпадают с первым словом списка, равно " + count);
+        count = 0;
+        tmpNode = list.getHead();
+        while (tmpNode.getNextNode() != null) {
+            tmpNode = tmpNode.getNextNode();
+        }
+        NodeString lastNode = tmpNode;
+        tmpNode = list.getHead();
+        while (tmpNode != null && tmpNode != lastNode) {
+            if (tmpNode.getValue().equalsIgnoreCase(lastNode.getValue())) {
+                count++;
+            }
+            tmpNode = tmpNode.getNextNode();
+        }
+        System.out.println("Количество слов в списке, которые совпадают с последним словом списка, равно " + count);
+    }
+
+// Метод проверки упорядоченности элементов списка по алфавиту
+    public static void task08(ListString list) {
+        if (isListOrdered(list)) {
+            System.out.println("Элементы в списке упорядочены по алфавиту");
+        } else {
+            System.out.println("Элементы в списке не упорядочены по алфавиту");
+        }
+    }
+
+    private static boolean isListOrdered(ListString list) {
+        NodeString tmpNode = list.getHead();
+        while (tmpNode.getNextNode() != null) {
+            if (!isStringsOrdered(tmpNode.getValue(), tmpNode.getNextNode().getValue())) {
+                return false;
+            }
+            tmpNode = tmpNode.getNextNode();
+        }
+        return true;
+    }
+
+    private static boolean isStringsOrdered(String firstStr, String secondStr) {
+        return firstStr.compareToIgnoreCase(secondStr) <= 0;
+    }
+
+// Метод определения вхождения списка L1 в L2
+    public static void task09(ListString l1, ListString l2) {
+        if (isListIncluded(l1, l2)) {
+            System.out.println("Список L1 входит в L2");
+        } else {
+            System.out.println("Список L1 не входит в L2");
+        }
+    }
+
+    private static boolean isListIncluded(ListString l1, ListString L2) {
+        NodeString tmpNodeL2 = L2.getHead();
+        while (tmpNodeL2 != null) {
+            NodeString tmpNodeL1 = l1.getHead();
+            NodeString tmpNode = tmpNodeL2;
+            while (tmpNodeL1 != null && tmpNode != null && tmpNodeL1.getValue().equalsIgnoreCase(tmpNode.getValue())) {
+                tmpNodeL1 = tmpNodeL1.getNextNode();
+                tmpNode = tmpNode.getNextNode();
+            }
+            if (tmpNodeL1 == null) {
+                return true;
+            }
+            tmpNodeL2 = tmpNodeL2.getNextNode();
+        }
+       return false;
+    }
+
+// Метод, переворачивающий список наоборот
+    public static void task10(ListString list) {
+        NodeString currentNode = list.getHead();
+        NodeString previousNode = null;
+        while (currentNode != null) {
+            NodeString nextNode = currentNode.getNextNode();
+            currentNode.setNextNode(previousNode);
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+        list.setHead(previousNode);
+/*
+  // Вариант 2.
+        if (list.getLength() > 1) {
+            NodeString currentNode = list.getHead();
+            while (currentNode.getNextNode().getNextNode() != null) {
+                currentNode = currentNode.getNextNode();
+            }
+            NodeString penultimate = currentNode;
+            NodeString firstNode = currentNode.getNextNode();
+            while (list.getHead() != penultimate.getNextNode()) {
+                penultimate.getNextNode().setNextNode(penultimate);
+                currentNode = list.getHead();
+                while (currentNode.getNextNode() != penultimate) {
+                    currentNode = currentNode.getNextNode();
+                }
+                penultimate = currentNode;
+            }
+            list.getHead().setNextNode(null);
+            list.setHead(firstNode);
+        }
+ */
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -43,7 +43,7 @@ public class Main {
         System.out.println(list.getLength());
 
  */
-        int[] arr = new int[] {1, 3, 7};
+        int[] arr = new int[]{1, 3, 7};
         List list = new List(new Node(arr[0]));
         for (int i = 1; i < arr.length; i++) {
             list.push(arr[i]);
@@ -51,19 +51,23 @@ public class Main {
 
         list.printList();
 
-/// Найти среднее арифметическое значение элементов списка.
+// Задание 1.
+// Найти среднее арифметическое значение элементов списка.
         System.out.println("Среднеарифметическое значение равно " + Exercises.task01(list));
 
+// Задание 2.
 // Перенести в начало списка его последний элемент.
         Exercises.task02(list);
         System.out.println("Переносим последний элемент в начало списка");
         list.printList();
 
+// Задание 3
 // Перенести в конец списка его первый элемент.
         Exercises.task03(list);
         System.out.println("Переносим начальный элемент в конец списка");
         list.printList();
 
+// Задание 4
 // Поменять местами первый и последний элементы списка.
 // Вариант 1.
         Exercises.task04_1(list);
@@ -71,11 +75,66 @@ public class Main {
         list.printList();
 
 // Вариант 2.
-
-     Exercises.task04_2(list);
+        Exercises.task04_2(list);
         System.out.println("Меняем местами первый и последний элемент (вариант 2)");
-     list.printList();
+        list.printList();
 
+        System.out.println();
 
+// Задание 5
+// Определить количество слов в списке, которые начинаются и заканчиваются на одну букву.
+        String[] arrTask5 = new String[]{"Аптека", "парашют", "потоп", "в", "угУ"};
+        ListString list05 = new ListString(arrTask5);
+        list05.printList();
+        Exercises.task05(list05);
+
+        System.out.println();
+
+// Задание 6
+// Проверить, что каждое следующее слово в списке начинается с последней буквы предыдущего.
+        String[] arrTask6 = new String[]{"task", "Kate", "bar", "red", "dad"};
+        ListString list06 = new ListString(arrTask6);
+        list06.printList();
+        Exercises.tack06(list06);
+
+        System.out.println();
+
+// Задание 7
+// Определить количество слов в списке, которые совпадают с первым (последним) словом списка.
+        String[] arrTask7 = new String[]{"tiger", "bird", "tiger", "bird", "bird", "tiger", "rabbit"};
+        ListString list07 = new ListString(arrTask7);
+        list07.printList();
+        Exercises.task07(list07);
+
+        System.out.println();
+
+// Задание 8
+// Проверить, упорядочены ли элементы списка по алфавиту.
+        String[] arrTask8 = new String[]{"aa", "Aa", "ab", "BA", "b"};
+        ListString list08 = new ListString(arrTask8);
+        list08.printList();
+        Exercises.task08(list08);
+
+        System.out.println();
+
+// Задание 9
+// Определить, входит ли список L1 в L2.
+        String[] arr1 = new String[] {"test", "task"};
+        String[] arr2 = new String[] {"task", "test", "dog",  "test", "task", "book"};
+        ListString l1 = new ListString(arr1);
+        ListString l2 = new ListString(arr2);
+        l1.printList();
+        l2.printList();
+        Exercises.task09(l1, l2);
+
+        System.out.println();
+
+// Задание 10
+// Перевернуть список наоборот.
+        String[] arrTas10 = new String[]{"aa", "bb", "cc", "dd"};
+        ListString list10 = new ListString(arrTas10);
+        list10.printList();
+        Exercises.task10(list10);
+        list10.printList();
     }
 }
